cad4daae1045adb403963913b6644148a3270e9b
SaneManiacMG <mmoroane@hotmail.com>
2023-01-17 11:02:51 +0200

    Added Product properties and annotations

---
+++ src/main/java/com/springguru/testrestful/models/Product.java
@@ -0,0 +1,47 @@
+package com.springguru.testrestful.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int productId;
+    @Column
+    private String productName;
+    @Column
+    private double amount;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, double amount) {
+        this.productId = productId;
+        this.productName = productName;
+        this.amount = amount;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
