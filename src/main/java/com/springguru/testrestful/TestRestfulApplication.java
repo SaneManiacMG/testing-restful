3641dfd78b5d3fd6aeb706dfafbb334bad957659
Qobete <104554205+Qobete@users.noreply.github.com>
2023-01-17 07:43:33 +0200

    first commit

---
+++ src/main/java/com/springguru/testrestful/TestRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.springguru.testrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springguru/testrestful/TestRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguru.testrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
